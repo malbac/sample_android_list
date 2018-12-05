2ec9740c10297e322c481ffae0e6e39487b05a2f
Martin Malobický <malobicky.m@gmail.com>
2018-12-05 17:37:02 +0100

    init

---
+++ app/src/main/java/sk/purpose/showoff/martin/sample/listapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package sk.purpose.showoff.martin.sample.listapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
